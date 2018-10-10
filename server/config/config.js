cc672dd0271167a2161f8c7ffb7584695c4c9d82
Gungo <gungo06@ufl.edu>
2018-10-10 11:46:11 -0400

    Added uri.

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
-//This file holds any configuration variables we may need
-//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
-//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+// This file holds any configuration variables we may need
+// 'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+// copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    
-    uri: , //place the URI of your mongo database here.
+    // mongodb://<dbuser>:<dbpassword>@ds113703.mlab.com:13703/game_gator
+    // dbuser: gg_admin , dbpassword: GamerGator3031
+    uri: 'mongodb://gg_admin:GamerGator3031@ds113703.mlab.com:13703/game_gator',
   },
   port: 8080
 };
