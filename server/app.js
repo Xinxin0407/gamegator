9ffd45d59df3cd5ba66b6835f14a98477eb0ca27
Gungo <gungo06@ufl.edu>
2018-10-10 11:10:04 -0400

    Added app.js file.

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var config = require('./config'),
+  mongoose = require('mongoose'),
+  express = require('./express');
+
+module.exports.start = function() {
+  var app = express.init();
+  // app.listen using process.env.PORT to run with heroku
+  // may not need, or can use localhost for testing
+  app.listen(process.env.PORT || 3000, function() {
+    console.log('App listening on port', process.env.PORT);
+  });
+};
