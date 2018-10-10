3e633fc4f75801b10fbda5c4d865ccfb76668f7d
Gungo <gdominguezgungo@gmail.com>
2018-10-10 12:13:29 -0400

    Fixed server port.

--- server/config/app.js
+++ server/config/app.js
@@ -6,9 +6,9 @@ module.exports.start = function() {
   var app = express.init();
   // app.listen using process.env.PORT to run with heroku
   // may not need, or can use localhost for testing
-  app.listen(process.env.PORT || 3000, function() {
-    var host = app.address().address;
-    var port = app.address().port;
+  var server = app.listen(process.env.PORT || 3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
     console.log('App listening at http://%s:%s', host, port);
   });
 };
