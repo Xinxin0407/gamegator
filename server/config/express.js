13cea485d231d01e1aab609b5e4c297ed825f6ef
Gungo <gungo06@ufl.edu>
2018-10-10 11:14:53 -0400

    Added express.js file.
    Need to add routers for events. Similar to listings as we did in proj4. Is there a need to serve static files?

---
+++ server/config/express.js
@@ -0,0 +1,38 @@
+var path = require('path'),
+  express = require('express'),
+  mongoose = require('mongoose'),
+  morgan = require('morgan'),
+  bodyParser = require('body-parser'),
+  config = require('./config'),
+  cors = require('cors');
+  // may need --> eventsRouter = require('../routes/events.server.routes')
+
+module.exports.init = function() {
+  // connect to database
+  mongoose.connect(config.db.uri, {
+    useNewUrlParser: true
+  });
+
+  // initialize app
+  var app = express();
+
+  // enable request logging for development debugging
+  app.use(morgan('dev'));
+
+  // body parsing middleware
+  app.use(bodyParser.json());
+
+  // cors
+  app.use(cors());
+
+  // Serve static files
+  
+  // Use the events router for requests to the api */
+
+  // go to homepage for all routes not specified */
+  app.all('/*', function(req, res) {
+    res.redirect('/');
+  });
+
+  return app;
+};
