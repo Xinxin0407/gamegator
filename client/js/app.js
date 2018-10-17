2636b2209d03a8498034839583de42f466f8ff70
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-10-17 09:19:16 -0400

    revert angular. apply vue

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,40 @@
 // similar to app.js for listings project
 
+
 /* register the modules the application depends upon here*/
+/*
 angular.module('events', []);
+*/
 
 /* register the application and inject all the necessary dependencies */
+/*
 var app = angular.module('gamergatorApp', ['events']);
+*/
+
+
+new Vue({
+  el: "#listings",
+  data: {
+    events: [
+      {
+        title: "Online tournament",
+        desc: "very cool event"
+      },
+      {
+        title: "Merch sale",
+        desc: "Lots to sell"
+      },
+      {
+        title: "Call of Duty 1v1",
+        desc: "no scope only"
+      }
+    ]
+  }
+});
+
+new Vue({
+  el: "#example",
+  data: {
+    greeting: "hi"
+  }
+});
