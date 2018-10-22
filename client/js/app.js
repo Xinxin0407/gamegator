592d005a71d1340efd528f898ae80d8351e092da
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-10-22 18:48:32 -0400

    Implement events_factory.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,34 +1,6 @@
 // similar to app.js for listings project
 
 
-//Events module
-new Vue({
-  el: "#listings",
-  data: {
-    events: [
-      {
-        title: "Online tournament",
-        desc: "very cool event"
-      },
-      {
-        title: "Merch sale",
-        desc: "Lots to sell"
-      },
-      {
-        title: "Call of Duty 1v1",
-        desc: "no scope only"
-      }
-    ]
-  }
-});
-
-new Vue({
-  el: "#example",
-  data: {
-    greeting: "hi"
-  }
-});
-
 //Tags module
 new Vue({
   el: "#tags",
