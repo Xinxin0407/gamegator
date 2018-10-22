09ccb765a71e7630cc5c5f571513773bebc5e680
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-10-22 18:48:32 -0400

    Implement events_factory.js

--- client/js/events_factory.js
+++ client/js/events_factory.js
@@ -1,4 +1,34 @@
-const Model = require("./../../server/models/events.server.model.js");
 
 
-exports.print = () => console.log(Model.find());
+events = []
+
+events = new Vue ({
+  el: "#listings",
+  data: {
+    events: [
+      {
+        title: "Online tournament",
+        desc: "very cool event"
+      },
+      {
+        title: "Merch sale",
+        desc: "Lots to sell"
+      },
+      {
+        title: "Call of Duty 1v1",
+        desc: "no scope only"
+      }
+    ]
+  }
+});
+
+const getEvents = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "/events", true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readystate === 4 && xhr.status === 200){
+      events.data.events = xhr.response.events;
+    }
+  };
+  xhr.send();
+}
