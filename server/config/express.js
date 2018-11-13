38bcc4033e2fa2052f987396670387d1978384a4
Gungo <gdominguezgungo@gmail.com>
2018-11-13 18:57:26 -0500

    Added user authentication branch.

--- server/config/express.js
+++ server/config/express.js
@@ -2,35 +2,73 @@ var path = require('path'),
   express = require('express'),
   mongoose = require('mongoose'),
   morgan = require('morgan'),
+  session = require('express-session'),
   bodyParser = require('body-parser'),
   config = require('./config'),
   cors = require('cors'),
-  eventsRouter = require('../routes/events.server.routes');
+  eventsRouter = require('../routes/events.server.routes'),
+  MongoStore = require('connect-mongo')(session);
 
 module.exports.init = function() {
   // connect to database
   mongoose.connect(config.db.uri, {
     useNewUrlParser: true
   });
+  var db = mongoose.connection;
+
+  //handle mongo error
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    // we're connected!
+  });
 
   // initialize app
   var app = express();
+
+  //use sessions for tracking logins
+  app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
+  }));
 
   // enable request logging for development debugging
   app.use(morgan('dev'));
 
   // body parsing middleware
   app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
 
   // cors
   app.use(cors());
 
   // Serve static files
-  app.use('/', express.static(__dirname + '/../../client'));
+  app.use('/', express.static(__dirname + '/../../templateLogReg'));
+  app.use('/Home', express.static(__dirname + '/../../client'));
   app.use('/MyEvents', express.static(__dirname + '/../../client/MyEvents'));
   app.use('/About', express.static(__dirname + '/../../client/About'));
   // Use the events router for requests to the api */
-  app.use(eventsRouter);
+  // app.use(eventsRouter);
+  // include routes
+  var userRoutes = require('../routes/users.server.routes.js');
+  app.use('/', userRoutes);
+
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+  // error handler
+  // define as the last app.use callback
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
+  });
 
   // go to homepage for all routes not specified */
   app.all('/*', function(req, res) {
--- server/models/users.server.model.js
+++ server/models/users.server.model.js
@@ -3,89 +3,66 @@
 /* Import mongoose and define any variables needed to create the schema */
 var mongoose = require('mongoose'),
   Schema = mongoose.Schema,
-  bcrypt = require('bcrypt'),
-  SALT_WORK_FACTOR = 10;
+  bcrypt = require('bcrypt');
 
 /* Create schema */
-var userSchema = new Schema({
-<<<<<<< HEAD
+var UserSchema = new Schema({
   username: {
     type: String,
     required: true,
-    index: { unique: true }
+    unique: true
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
     required: true
   },
-  profile_img: {
-    data: Buffer,
-    contentType: String
+  passwordConf: {
+    type: String,
+    required: true
   },
+  admin: Boolean,
   created_at: Date,
   updated_at: Date
-=======
-    username: {
-        type: String,
-        required: true,
-        index: { unique: true }
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profile_img: {
-        data: Buffer,
-        contentType: String
-    },
-    admin: Boolean,
-    created_at: Date,
-    updated_at: Date
->>>>>>> c9b0d7ef0ab02e3cde530a3a5b1a3258c98acac7
 });
 
-/* 'pre' function that adds the updated_at (and created_at if not already there) property */
-userSchema.pre('save', function(next) {
-  var user = this;
-  var currentTime = new Date;
-  user.updated_at = currentTime;
-  if (!user.created_at) {
-    user.created_at = currentTime;
-  }
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
+//authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({ email: email })
+    .exec(function (err, user) {
+      if (err) {
+        return callback(err)
+      } else if (!user) {
+        var err = new Error('User not found.');
+        err.status = 401;
+        return callback(err);
+      }
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      })
     });
-  });
-  next();
+}
+
+// hashing a password before saving it to the database
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
 });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-/* Instantiate Mongoose model */
-var User = mongoose.model('User', userSchema);
-
-/* Export to make it available to other parts of application */
+// Instantiate and export Mongoose model
+var User = mongoose.model('User', UserSchema);
 module.exports = User;
