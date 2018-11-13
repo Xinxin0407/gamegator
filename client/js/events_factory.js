67a33783f0097c9e0c0aebe8a8f18d127b38367b
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-11-13 00:30:42 -0500

    Beautify events

--- client/js/events_factory.js
+++ client/js/events_factory.js
@@ -4,6 +4,30 @@ const events = new Vue ({
   el: "#listings",
   data: {
     events: []
+  },
+  methods: {
+    appendDescription: function (index){
+      this.events[index].address = this.events[index].address + " ";
+    },
+    toggleInfo: function (event){
+      event.moreInfo = event.moreInfo === false;
+    },
+
+    moreInfo: function (event) {
+      return event.moreInfo;
+    }
+
+  },
+
+  computed: {
+    filteredList: function (keywords){
+      return this.events.filter(event => {
+        for (property in event){
+          if (property.toLowerCase().includes(keywords.toLowerCase())) return true;
+        }
+        return false;
+      })
+    }
   }
 });
 
@@ -13,8 +37,12 @@ const getEvents = () => {
   xhr.open("GET", "/events", true);
   xhr.onreadystatechange = () => {
     console.log(xhr);
-    if (xhr.readyState === 4)
-      events._data.events = JSON.parse(xhr.response);
+    if (xhr.readyState === 4) {
+        events._data.events = JSON.parse(xhr.response);
+        for (let i = 0; i < events._data.events.length; i++){
+          events._data.events[i].moreInfo = false;
+        }
+    }
   };
   xhr.send();
 };
