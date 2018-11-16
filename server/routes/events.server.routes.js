8b144a8de41f7dc404081a912022c443d730b7be
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-11-16 16:38:07 -0500

    Fix routes from heroku.

--- server/routes/events.server.routes.js
+++ server/routes/events.server.routes.js
@@ -10,7 +10,7 @@ var events = require('../controllers/events.server.controller.js'),
   These method calls are responsible for routing requests to the correct request handler.
   Take note that it is possible for different controller functions to handle requests to the same route.
  */
-router.route('https://gamegator-demo2.herokuapp.com/events')
+router.route('/events')
   .get(events.list)
   .post(events.create);
 
