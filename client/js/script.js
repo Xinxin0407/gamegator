8151d5a78abbe7ceec22c4615253d20070971a43
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-11-16 21:11:50 -0500

    new javascript
    coookie, dom

--- client/js/script.js
+++ client/js/script.js
@@ -24,3 +24,62 @@ $(function(){
     e.preventDefault(); // stop form submission
   });
 });
+
+
+//        DOM
+//-----------------------------------------------------------------------------
+function getElement(id){
+  return document.getElementById(id);
+}
+
+function editText(id, text){
+  getElement(id).innerText = text;
+}
+
+function editHTML(id, html){
+  getElement(id).innerHTML = html;
+}
+
+
+
+//        COOKIES
+//-------------------------
+function setCookie(key, value) {
+    document.cookie = key + "=" + value + ";path=/";
+}
+
+function clearCookie(key){
+  setCookie(key, "");
+}
+
+function getCookie(key) {
+    var name = key + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+//        AJAX
+//------------------------------------------------------------
+function sendXHR(method, path, body, callback){
+  let xhr = new XMLHttpRequest();
+
+  xhr.open(method, path);
+
+  xhr.onreadystatechange(() => {
+    if (xhr.readyState === 4){
+      if (callback) callback();
+    }
+  });
+
+  xhr.send();
+}
