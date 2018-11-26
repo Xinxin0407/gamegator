80479dd77bd6b4951252363e316433148c873e48
Xinxin0407 <42815551+Xinxin0407@users.noreply.github.com>
2018-11-26 21:46:34 +0800

    Add login and register function

--- server/controllers/users.server.controller.js
+++ server/controllers/users.server.controller.js
@@ -94,6 +94,57 @@ exports.delete_user = function(req, res) {
   });
 };
 
+exports.user_login = function (req, res, next) {
+  if (req.body.username && req.body.password) {
+    User.authenticate(req.body.username, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong username or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } 
+  else {
+    var err = new Error('All fields required.');
+    err.status = 400;
+    return next(err);
+  }
+}
+// user register
+exports.user_reigster = function (req, res, next) {
+  // confirm that user typed same password twice
+  if (req.body.password !== req.body.passwordConf) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords do not match");
+    return next(err);
+  }
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+    var userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      passwordConf: req.body.passwordConf,
+    }
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        req.session.userId = user._id;
+        return next();
+      }
+    });
+  }
+}
 /*
   Middleware: find a listing by its ID, then pass it to the next request handler.
   HINT: Find the listing using a mongoose query,
