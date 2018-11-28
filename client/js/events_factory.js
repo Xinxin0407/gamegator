1946c138b975ac7e65852b0b03ec81a096f77634
Aeyzechiah Vasquez <azvasquez99@gmail.com>
2018-11-28 10:18:11 -0500

    Add filter by tags
    BUG: checking the tag does not immediately update the list of events

--- client/js/events_factory.js
+++ client/js/events_factory.js
@@ -28,18 +28,49 @@ const events = new Vue ({
     filteredList: function (){
       let keywords = search._data.search;
       return this.events.filter(event => {
-        if (event.name.toLowerCase().includes(keywords.toLowerCase())) return true;
         let match = false;
+        if (event.name.toLowerCase().includes(keywords.toLowerCase())) match = true;
         event.Keywords.map(ekw => {
           match |= (ekw && ekw.name && ekw.name.toLowerCase().includes(keywords));
         });
 
+        const TAGS = [
+          'role-playing', 'board-games', 'pc-games', 'xbox-games', 'ps4-games', 'card-games', 'no-buy-in', 'tournaments', 'casual'
+        ]
+
+        for (tagg in TAGS){
+          const tag = TAGS[tagg];
+          const tagtrue = getElement(tag).checked;
+          if (tagtrue) match &= eventHasKeyword(event, tag.replace("-", " "));
+        }
+
         //match |= event.description.contains(keywords);
+        /*
+        if (getElement("role-playing").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("role-playing"));
+        if (getElement("board-games").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("board-games"));
+        if (getElement("pc-games").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("pc-games"));
+        if (getElement("xbox-games").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("xbox-games"));
+        if (getElement("ps4-games").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("ps4-games"));
+        if (getElement("card-games").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("card-games"));
+        if (getElement("no-buy-in").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("no-buy-in"));
+        if (getElement("tournaments").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("tournaments"));
+        if (getElement("casual").checked) match &= event.Keywords && event.Keywords.map(kw => kw && kw.name.includes("casual"));
+        */
+
+        console.log(getElement("casual").checked);
         return match;
       })
     }
   }
 });
+
+function eventHasKeyword(event, keyword){
+  let match = false;
+  event.Keywords.map(kw => {
+    if (kw && kw.name && (kw.name.toLowerCase().includes(keyword) || keyword.toLowerCase().includes(kw.name.toLowerCase()))) match = true;
+  })
+  return match;
+}
 
 const getEvents = (callback) => {
   console.log(events);
